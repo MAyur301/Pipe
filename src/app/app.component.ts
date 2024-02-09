89c6fd97e1c071959539dfbcff9b33fe7eac0639
MAyur301 <bhavsarmayur786@gmail.com>
2024-02-09 15:12:24 +0530

    pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pipe';
+
+  servers =[
+    {
+      instanceType: 'medium',
+      name : 'Production server',
+      status:'stable',
+      started: new Date(13,1,2017)
+    },
+    {
+      instanceType: 'large',
+      name : 'User Data',
+      status:'stable',
+      started: new Date(15,1,2017)
+    },
+    {
+      instanceType: 'small',
+      name : 'Development server',
+      status:'offline',
+      started: new Date(15,1,2017)
+    },
+  ];
+  filter ='ER'
+  getStatusClasses(server:{instanceType:string,name : String,status: string ,started: Date})
+  {
+
+    return{
+      'list-group-item-success':server.status ==='stable',
+      'list-group-item-warning':server.status ==='offline',
+      'list-group-item-danger':server.status ==='critical'
+    }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { shorntpipe } from './shorten.pipe';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    shorntpipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
